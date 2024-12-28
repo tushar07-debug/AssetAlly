60f0551f4cbfce925690d9637513c306b6ed526e
tushar07-debug <tusharsingh102003@gmail.com>
2024-12-28 20:56:39 +0530

    Mobie-view

---
+++ app/(root)/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
+const Home=()=> {
+    const loggedIn = {firstName:'Tushar'};
+  return (
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox
+                  type="greeting"
+                  title="Welcome"
+                  user={loggedIn?.firstName||'Guest'}
+                  subtext="Access and manage your account and transcation efficiently."
+                />
+            </header>
+
+            <TotalBalanceBox
+              accounts={[]}
+              totalBanks={1}
+              totalCurrentBalance={1250.35}
+            />
+        </div>
+    </section>
+  )
+}
+
+export default Home
